93efd90bb1a092658e43093e2d383c22d3d62ea9
Atridge D'Costa <atridge.dcosta@blueflag.com.au>
2015-01-27 15:41:28 +1100

    Optimised

--- utils/Maths.js
+++ utils/Maths.js
@@ -9,11 +9,17 @@ var _ = require('lodash');
 
 var Maths = {
     extent: function (data) {
+        var sorted = _.sortBy(data);
+
         // Sorts array and returns as [first, last]
-        var sorted = data.sort(function (a,b) {
-            return a - b;               
-        });
-        return [sorted[0], sorted[sorted.length - 1]];
+        // var sorted = data.sort(function (a,b) {
+        //     return a - b;               
+        // });
+
+        var min = sorted[0];
+        var max = sorted[sorted.length - 1];
+
+        return [min, max];
     },
     normalize: function (rawValue, min, max) {
         // return 100 - Math.round((ammount / max) * 100);
