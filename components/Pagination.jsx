ad2d2e53ef2ed576849023870471ffe6d1b86ec2
Allan Hortle <allan.hortle@blueflag.com.au>
2015-02-25 11:29:35 +1100

    basic pagination

---
+++ components/Pagination.jsx
@@ -0,0 +1,61 @@
+/** @jsx React.DOM */
+var React = require('react');
+var _ = require('lodash');
+var paginate = require('../utils/Paginate');
+
+var Pagination = React.createClass({
+    displayName: 'Pagination',
+    mixins: [
+        require('../mixins/ClassMixin')
+    ],
+    propTypes: {
+        ammount: React.PropTypes.number,
+        length: React.PropTypes.array,
+        page: React.PropTypes.number
+    },
+    getDefaultProps: function () {
+        return {
+            length: 0,
+            page: 0,
+            ammount: 25  
+        };
+    },
+    onClick(page, e) {
+        if(this.props.onClick) {
+            this.props.onClick(e, page);
+        }
+    },
+    render: function () {       
+        var numberOfPages = Math.ceil(this.props.length / this.props.ammount);
+        this.classes = this.ClassMixin_getClass('Pagination');
+        return <div className={this.classes.className}>
+            {this.renderButtons(numberOfPages)}
+            <ul className={this.classes.child('list')}>
+                {_(numberOfPages).range().map(this.renderItem).value()}
+            </ul>
+        </div>
+    },
+    renderButtons(numberOfPages) {
+        var next, prev;
+        var buttonClass = this.classes.child('button');
+
+        if(this.props.page > 0) {
+            prev = <button className={`${buttonClass} ${buttonClass}-previous`} onClick={this.onClick.bind(this, this.props.page - 1)}>prev</button>;
+        }
+        
+        if(this.props.page <= numberOfPages) {
+            next = <button className={`${buttonClass} ${buttonClass}-next`} onClick={this.onClick.bind(this, this.props.page + 1)}>next</button>;
+        }
+        
+        return (
+            <div>{prev}{next}</div>
+        );
+    },
+    renderItem(item) {
+        var className = this.classes.child('listItem');
+        var isActive = (item === this.props.page) ? 'is-active' : '';
+        return <li className={`${className} ${isActive}`} onClick={this.onClick.bind(this, item)}><a>{item}</a></li>;
+    }
+});
+
+module.exports = Pagination;
