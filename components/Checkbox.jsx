d9f5cb97fc20a98de4bc66412ee77d596103ca43
Allan Hortle <allan.hortle@blueflag.com.au>
2015-03-11 10:16:00 +1100

    checkbox

--- components/Checkbox.jsx
+++ components/Checkbox.jsx
@@ -11,23 +11,30 @@ var Checkbox = React.createClass({
         onChange: React.PropTypes.func
     },
     mixins: [ClassMixin, PureRenderMixin],
+    onClick: function (ee) {
+        // Manual toggle hack. Somtimes the lable clickevent will auto scroll to nowhere...
+        var checkbox = this.refs.checkbox.getDOMNode();
+        checkbox.checked = !checkbox.checked;
+        ee.preventDefault();
+    },
     render: function () {
         var classes = this.ClassMixin_getClass('Checkbox');
         return (
-            <label className={classes.className}>
-                <Input 
+            <label className={classes.className} onClick={this.onClick}>
+                <Input
                     type="checkbox"
+                    ref="checkbox"
                     className={classes.child('input')}
                     name={this.props.name}
                     checked={this.props.checked}
                     defaultChecked={this.props.checked || false}
                     onChange={this.props.onChange}
-                />                  
+                />
                 <span className={classes.child("label")}>{this.props.children}</span>
             </label>
         );
     },
-    
+
 });
 
 module.exports = Checkbox;
