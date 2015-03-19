25c9cd9c34ad63e405b19ca4dc9bc2741002e6ff
Atridge D'Costa <atridge.dcosta@blueflag.com.au>
2015-03-20 10:17:26 +1100

    No longer return an empty object

--- utils/UrlStore.js
+++ utils/UrlStore.js
@@ -146,7 +146,7 @@ UrlStore.prototype = _.defaults(UrlStore.prototype, {
     },
     patternMatch: function(str) {
         var pattern = urlPattern.newPattern(str);
-        return pattern.match(this.getHash()) || {};
+        return pattern.match(this.getHash());
     }
 
 });
