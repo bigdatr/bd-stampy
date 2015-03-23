d57e9743a4feafcbe27c3373707d5b45b3da9e46
allanhortle <allanhortle@gmail.com>
2015-03-23 15:13:33 +1100

    error

--- scripts/deploy_docs_from_travis.sh
+++ scripts/deploy_docs_from_travis.sh
@@ -1,6 +1,6 @@
 # #!/bin/bash
 git config user.name "allanhortle"
 git config user.email "allanhortle@gmail.com"
-git subtree push --prefix docs "https://${GH_TOKEN}@github.com/bigdatr/bd-stampy.git gh-pages"
+git subtree push --prefix docs "https://${GH_TOKEN}@github.com/bigdatr/bd-stampy.git" gh-pages
 echo "Pushed new documentation successfully."
 exit 0
