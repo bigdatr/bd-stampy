dab91b9877b242cedabd8a04d34f511f8ee429a3
Allan Hortle <allan.hortle@blueflag.com.au>
2015-03-23 12:52:48 +1100

    better deploy script for docs

--- scripts/deploy_docs_from_travis.sh
+++ scripts/deploy_docs_from_travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Setting up git origin"
-git remote add travis "https://${GH_TOKEN}@${GH_REF}" docs > /dev/null 2>&1 || exit 1
-git subtree push --prefix docs travisorigin gh-pages > /dev/null 2>&1 || exit 1
+git remote add travis "https://${GH_TOKEN}@${GH_REF}"
+git subtree push --prefix docs travis gh-pages
 echo "Pushed new documentation successfully."
 exit 0
