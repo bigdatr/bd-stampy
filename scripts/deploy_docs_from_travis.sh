0c465317dcfe1f3b732c355a42053179b6ceb7d4
allanhortle <allanhortle@gmail.com>
2015-03-23 15:11:21 +1100

    deploy script

--- scripts/deploy_docs_from_travis.sh
+++ scripts/deploy_docs_from_travis.sh
@@ -1,7 +1,6 @@
 # #!/bin/bash
 git config user.name "allanhortle"
 git config user.email "allanhortle@gmail.com"
-git push "https://${GH_TOKEN}@github.com/bigdatr/bd-stampy.git" `git subtree split --prefix docs master`:gh-pages --force
+git subtree push --prefix docs "https://${GH_TOKEN}@github.com/bigdatr/bd-stampy.git gh-pages"
 echo "Pushed new documentation successfully."
 exit 0
-
