cdd148abedbdc6681f0b63114b904f3cc52fc2c7
allanhortle <allanhortle@gmail.com>
2015-03-23 13:33:53 +1100

    secure test

--- scripts/deploy_docs_from_travis.sh
+++ scripts/deploy_docs_from_travis.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
-echo "Setting up git origin"
-git remote add travis "https://${GH_TOKEN}@${GH_REF}"
-git subtree push --prefix docs travis gh-pages
+# #!/bin/bash
+# echo "Setting up git origin"
+# curl -H 'Authorization: token ${GH_TOKEN}' https://${GH_TOKEN}@${GH_REF}
+# git remote add travis "https://${GH_TOKEN}@${GH_REF}"
+# git subtree push --prefix docs travis gh-pages
+# echo "Pushed new documentation successfully."
+# exit 0
+
+git config user.name "allanhortle"
+git config user.email "allanhortle@gmail.com"
+git subtree push --prefix docs "https://${GH_TOKEN}@github.com/bigdatr/bd-stampy.git" gh-pages
 echo "Pushed new documentation successfully."
 exit 0
+
