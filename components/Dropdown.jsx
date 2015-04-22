39ab0cb1becfb43808480d5203348fccfc3f8128
allanhortle <allanhortle@gmail.com>
2015-04-22 13:37:05 +1000

    dropdown component

---
+++ components/Dropdown.jsx
@@ -0,0 +1,41 @@
+/*global window*/
+var React = require('react');
+
+var Dropdown = React.createClass({
+    displayName: 'Dropdown',
+    mixins: [
+        require('../mixins/ClassMixin')
+    ],
+    getInitialState() {
+        return {
+            active: false
+        };
+    },
+    componentWillMount: function () {
+        window.addEventListener("click", this.closeDropdown, false);
+    },
+    componentWillUnmount: function () {
+        window.removeEventListener("click", this.closeDropdown, false);
+    },
+    openDropdown(e){
+        this.setState({active: true});
+    },
+    closeDropdown(e) {
+        if(!this.refs.button.getDOMNode().contains(e.target)) {
+            this.setState({active: false});
+        }
+    },
+    render() {
+        var classes = this.ClassMixin_getClass('Dropdown').is(this.state.active, 'active');
+        return (
+            <div className={classes.className} onClick={this.openDropdown}>
+                <div ref="button" className={classes.child('button')}>{this.props.button}</div>
+                <ul className={classes.child('list')}>
+                    {this.props.children}
+                </ul>
+            </div>
+        );
+    }
+});
+
+module.exports = Dropdown;
