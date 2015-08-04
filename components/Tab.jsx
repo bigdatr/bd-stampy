289b4b3075cf108e4688deed91eb8468575adbea
allanhortle <allanhortle@gmail.com>
2015-08-04 16:20:06 +1000

    Remove refs to icon from Tab

--- components/Tab.jsx
+++ components/Tab.jsx
@@ -1,35 +1,24 @@
-
 /**
  * Tab
  *
  * @param {String} example  <p>See TabView for demo</p>
  */
-var React = require('react');
-var Icon = require('./Icon.jsx');
-var PureRenderMixin = require('react/addons').addons.PureRenderMixin;
-
+var React = require('react/addons');
 
 var Tab = React.createClass({
     displayName: 'Tab',
     type: 'Tab',
-    mixins: [PureRenderMixin],
+    mixins: [PureRenderMixin.addons.PureRenderMixin],
     propTypes: {
-        icon: React.PropTypes.string,
         text: React.PropTypes.string
     },
     render: function() {
         return (
             <span className={this.props.className}>
-                <span>{this.renderIcon()}</span>
                 <span className="Tab_text">{this.props.text}</span>
                 {this.props.children}
             </span>
         );
-    },
-    renderIcon: function () {
-        if (this.props.icon) {
-            return <Icon name={this.props.icon} size="small"/>;
-        }
     }
 });
 
